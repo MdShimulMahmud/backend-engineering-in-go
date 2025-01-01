05e6aaa526b445640e2b13c3c13f1828aa00239d
MdShimulMahmud <shimulmahmud18@gmail.com>
2025-01-01 22:35:28 +0600

    mongo-db driver added

--- frameworks/fiber/todo-apps/main.go
+++ frameworks/fiber/todo-apps/main.go
@@ -1,51 +1,65 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cache"
-	"github.com/gofiber/fiber/v2/middleware/compress"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/csrf"
-	"github.com/gofiber/fiber/v2/middleware/etag"
-	"github.com/gofiber/fiber/v2/middleware/favicon"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/monitor"
-	"github.com/gofiber/fiber/v2/middleware/pprof"
-	"github.com/gofiber/fiber/v2/middleware/proxy"
-	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/fiber/v2/middleware/requestid"
-	"github.com/gofiber/fiber/v2/middleware/rewrite"
-	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	docs := "www.mongodb.com/docs/drivers/go/current/"
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
+			"See: " + docs +
+			"usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+
+	coll := client.Database("sample_mflix").Collection("movies")
+	title := "Back to the Future"
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).
+		Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", title)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+
 	app := fiber.New()
 
-	app.Use(logger.New())
-	app.Use(recover.New())
-	app.Use(requestid.New())
-	app.Use(limiter.New())
-	app.Use(cors.New())
-	app.Use(compress.New())
-	app.Use(pprof.New())
-	app.Use(cache.New())
-	app.Use(csrf.New())
-	app.Use(etag.New())
-	app.Use(favicon.New())
-	app.Use(monitor.New())
-	app.Use(proxy.New(proxy.Config{
-		// Add appropriate configuration here
-	}))
-	app.Use(requestid.New())
-	app.Use(session.New())
-	app.Use(rewrite.New())
-	app.Use(limiter.New())
-	app.Use(pprof.New())
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":5000")
 }
