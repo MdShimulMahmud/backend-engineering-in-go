c3a02cefa0291f2ac67a2fac17307086fe690064
MdShimulMahmud <shimulmahmud18@gmail.com>
2025-01-03 22:59:34 +0600

    added

--- frameworks/fiber/todo-apps/main.go
+++ frameworks/fiber/todo-apps/main.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"log"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/golang-restapi/todos/config"
+	"github.com/golang-restapi/todos/controllers"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+	
+	if err:= config.ConnectDB() ; err ==nil{
+
+		fmt.Println("Connected to MONGODB ATLAS")
 	}
-	uri := os.Getenv("MONGODB_URI")
-	docs := "www.mongodb.com/docs/drivers/go/current/"
-	if uri == "" {
-		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
-			"See: " + docs +
-			"usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().
-		ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+
 
 	app := fiber.New()
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Server in running"})
-	})
+	app.Get("/", controllers.GetTodos)
 
-	app.Listen(":5000")
+	log.Fatal(app.Listen(":5000"))
+	
 }
